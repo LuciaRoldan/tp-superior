af2e7cb6f434e8f4f116cbb3334197392847e39c
Unknown <rociochipian@gmail.com>
2018-11-08 23:56:48 -0300

    anda todito ?

--- EjemploVentana/src/EquationProcessor/GaussSeidel.java
+++ EjemploVentana/src/EquationProcessor/GaussSeidel.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 public class GaussSeidel {
 
-	public ArrayList<Vector> gaussSeidelIterations(Matriz matriz, Vector vectorCoeficientes, Vector vectorInicial, double criterioDeParo){
+	public Result gaussSeidelIterations(Matriz matriz, Vector vectorCoeficientes, Vector vectorInicial, double criterioDeParo){
+		
+		Result resultado = new Result();
 		
 		ArrayList<Vector> vectores = new ArrayList<Vector>();
 				
@@ -23,21 +25,39 @@ public class GaussSeidel {
 		Vector anterior = vectorInicial;
 		
 		vectores.add(anterior);
+		resultado.agregarResultado(anterior);
 		
 		Vector auxVector = dMinusLInverseMultiplyU.multiplyVector(anterior).plusVector(dMinusLInverseMultiplyB);
 		
 		vectores.add(auxVector);
+		resultado.agregarResultado(auxVector);
 						
 		while(! cumpleCriterioDeParo(anterior, auxVector, criterioDeParo)) {
 			
+			//va aca?
+			Vector errores = anterior.minus(auxVector);
+			resultado.agregarError(errores);
+			
+			Double normaInfinito = anterior.minus(auxVector).normaInfinito();
+			resultado.agregarInfinito(normaInfinito);			
+			
 			anterior = auxVector;
 			auxVector = dMinusLInverseMultiplyU.multiplyVector(anterior).plusVector(dMinusLInverseMultiplyB);
+			
 			vectores.add(auxVector);
+			resultado.agregarResultado(auxVector);
 		}
+		
+		//como no entre al while, entonces no agregue errores y norma a resultado, agrego antes de terminar
+		Vector errores = anterior.minus(auxVector);
+		resultado.agregarError(errores);
+				
+		Double normaInfinito = anterior.minus(auxVector).normaInfinito();
+		resultado.agregarInfinito(normaInfinito);
 		
 		vectores.forEach(v -> v.mostrar());
 		
-		return vectores;			
+		return resultado;			
 	}
 
 	private boolean cumpleCriterioDeParo(Vector anterior, Vector auxVector, double criterioDeParo) {		
